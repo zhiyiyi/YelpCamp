3c46ddafdb5c3a95c42bf946d2122563d5919699
zhiyiyi <chenzhiyi401@gmail.com>
2023-10-25 13:18:55 -0700

    Set up ejs view, MongoDB model and connect to database

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+})
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp);
+})
+
+app.listen(3000, () => {
+    console.log("Serving on port 3000");
+})
